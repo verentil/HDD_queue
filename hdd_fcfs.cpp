a8d71af273dd4c0630d2b72516c2246134d2bb3e
verentil <verentil@gmail.com>
2015-11-05 14:20:06 +0400

    Base class added

--- hdd_fcfs.cpp
+++ hdd_fcfs.cpp
@@ -1,77 +1,10 @@
 #include "hdd_fcfs.h"
 
-const int hdd_fcfs::rpm = 7200;
-const double hdd_fcfs::track_to_track_seek_time = 1.2;                // Read 1 ms. Write 1.2 ms.
-
-hdd_fcfs::hdd_fcfs( int hdd_size_in_GB )
-{
-    sector_count = hdd_size_in_GB * 1e+09                           // 1GB = 1e+09 Bytes.
-                    / ( hdd_sector_size_in_byte * hdd_track_count );
-    one_track_change_time = track_to_track_seek_time * 2 / hdd_track_count ;
-    one_rate_time = 6e+04 / rpm;                                    // 1 minute = 6e+04 ms.
-}
-
-double hdd_fcfs::next_poisson_time_step()
-{
-    double poisson_probability = (double) ( rand() % RAND_MAX + 1 ) / RAND_MAX;
-    double io_time_step = - log( poisson_probability ) * 1000 / poisson_intension;
-    return io_time_step;
-}
-
-void hdd_fcfs::add_io_task_to_que( double next_io_time )
-{
-    if ( controller_que.size() < hdd_controller_que_size )
-    {
-        unsigned int next_io_track = random_uint() % ( hdd_track_count + 1 ) - random_uint.min();
-        unsigned int next_io_sector = random_uint() % ( sector_count + 1 );
-        controller_que.insert( pair< double, pair< unsigned int, unsigned int > >
-                                (   next_io_time,
-                                    pair< unsigned int, unsigned int >
-                                    (   next_io_track,
-                                        next_io_sector )
-                                ) );
-        if ( controller_que.size() > max_controller_que_size )
-            max_controller_que_size = controller_que.size();
-    } else {
-//        std::cout << "Quit from que size!" << std::endl;
-        ++missed_io;
-    }
-}
-
 map< double, pair< unsigned int, unsigned int > >::iterator
     hdd_fcfs::get_io_task_from_que()
 {
     auto next_io = controller_que.begin();
     return next_io;
-}
-
-double hdd_fcfs::move_heads( unsigned int new_track, unsigned int new_sector )
-{
-    double track_changing_time = abs( new_track - current_track ) * one_track_change_time;
-    double sector_waiting_time = abs( new_sector - current_sector ) * one_rate_time
-                                    / sector_count;
-    double elapsed_time = track_changing_time + sector_waiting_time;
-
-    current_track = new_track;
-    current_sector = new_sector;
-
-    return elapsed_time;
-}
-
-void hdd_fcfs::update_avg_time( double last_io_time )
-{
-    auto appropriate_unit = avg_time_for_track.find( current_track );
-    if ( appropriate_unit == avg_time_for_track.end() )
-    {
-        avg_time_for_track.insert( pair< unsigned int, pair< double, unsigned long long > >
-                                    (   current_track,
-                                        pair< double, unsigned long long >( last_io_time, 1 )
-                                    ) );
-    } else
-    {
-        appropriate_unit->second.first += last_io_time;
-        appropriate_unit->second.second++;
-    }
 }
 
 double hdd_fcfs::perform_next_io()
@@ -89,29 +22,15 @@ double hdd_fcfs::perform_next_io()
     return elapsed_time;
 }
 
-double hdd_fcfs::get_avg_io_seek()
-{
-    double overall_io_time = 0;
-    unsigned long long overall_io_count = 0;
-    for ( auto track_time : avg_time_for_track )
-    {
-        overall_io_time += track_time.second.first;
-        overall_io_count += track_time.second.second;
-    }
-    double avg_io_seek = overall_io_time / overall_io_count;
-    return avg_io_seek;
-}
-
 double hdd_fcfs::test_avg_seek( unsigned long long attempt_count )
 {
     double next_io_time = current_time;
-    while ( attempt_count > 0 )
+    for ( unsigned long long i = 0 ; i < attempt_count ; ++i )
     {
         while ( next_io_time <= current_time )
         {
             next_io_time += next_poisson_time_step();
             add_io_task_to_que( next_io_time );
-//            std::cout << "next_io_time: " << next_io_time << std::endl;
         }
         while ( next_io_time > current_time )
         {
@@ -123,8 +42,6 @@ double hdd_fcfs::test_avg_seek( unsigned long long attempt_count )
             double elapsed_time = perform_next_io();
             current_time += elapsed_time;
         }
-        --attempt_count;
-//        std::cout << "attempt count: " << attempt_count << std::endl;
     }
     return get_avg_io_seek();
 }
--- hdd_fcfs.h
+++ hdd_fcfs.h
@@ -1,11 +1,10 @@
 #ifndef ACRONIS_HDD_FCFS_H
 #define ACRONIS_HDD_FCFS_H
 
-#include <cmath>
-#include <cstdlib>
 #include <iostream>
 #include <map>
-#include <random>
+
+#include "hdd_base.h"
 
 using std::map;
 using std::pair;
@@ -16,38 +15,19 @@ extern const unsigned int hdd_track_count;
 extern const unsigned int hdd_sector_size_in_byte;
 extern const unsigned int poisson_intension;
 
-class hdd_fcfs
+class hdd_fcfs: protected hdd_base
 {
-    static const int rpm;
-    static const double track_to_track_seek_time;
-    double one_rate_time;
-    double one_track_change_time;
-    unsigned int sector_count;
-    unsigned int current_track = 0;
-    unsigned int current_sector = 1;
-    double current_time = 0;
-    unsigned long long missed_io = 0;
-    unsigned int max_controller_que_size = 0;
-    map< double, pair< unsigned int, unsigned int > > controller_que;
-    map< unsigned int, pair< double, unsigned long long > > avg_time_for_track;
-    default_random_engine random_uint;
-
     hdd_fcfs(){};                                 // HDD should have a size.
-    double next_poisson_time_step();
-    void add_io_task_to_que( double );
     map< double, pair< unsigned int, unsigned int > >::iterator
         get_io_task_from_que();
-    double move_heads( unsigned int, unsigned int );
     double perform_next_io();
-    void update_avg_time( double );
 public:
-    explicit hdd_fcfs( int );
+    explicit hdd_fcfs( int hdd_size_in_GB ) : hdd_base( hdd_size_in_GB ) {};
     ~hdd_fcfs()
     {
         std::cout << "missed_io: " << missed_io << std::endl;
         std::cout << "max_controller_que_size: " << max_controller_que_size << std::endl;
     };
-    double get_avg_io_seek();
     double test_avg_seek( unsigned long long );
 };
 
